515ef76abdd12d1d40739f23110fca703c399186
Dan Mazo <danmazo32@gmail.com>
2017-12-03 02:05:20 +0000

    added bubblesort

--- find/generate.c
+++ find/generate.c
@@ -8,7 +8,7 @@
  * where n is number of pseudorandom numbers to print
  * and s is an optional seed
  */
- 
+
 #define _XOPEN_SOURCE
 
 #include <cs50.h>
@@ -21,17 +21,17 @@
 
 int main(int argc, string argv[])
 {
-    // TODO: comment me
+    // If the arguments passed are more than 2 or less than 1, print the proper usage.
     if (argc != 2 && argc != 3)
     {
         printf("Usage: ./generate n [s]\n");
         return 1;
     }
 
-    // TODO: comment me
+    // since by default arguments are passed as a string datatype, atoi() function converts it into a numerical datatype and asssigns it to n
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    // checks if seed is specified, it seems that the string from the command line argument is converted to integer and converted to Long int, if no seed then, current time is used as seed
     if (argc == 3)
     {
         srand48((long) atoi(argv[2]));
@@ -41,7 +41,7 @@ int main(int argc, string argv[])
         srand48((long) time(NULL));
     }
 
-    // TODO: comment me
+    // from the first argument, it is passed as the iteration, then drand48 gets input bet 0 and 1, then multiplies to limit to get the theoretical percentage, converted to int which causes it to lose precision and prints it to std output
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", (int) (drand48() * LIMIT));
--- find/helpers.c
+++ find/helpers.c
@@ -3,7 +3,7 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
+
 #include <cs50.h>
 
 #include "helpers.h"
@@ -14,6 +14,7 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+
     return false;
 }
 
@@ -23,5 +24,43 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
+    //Implementing bubblesort with a T(n) = O(n^2)
+
+    //If length is 1 then return the array
+    if(n <= 1){
+        return;
+    }
+
+    //Initializing swap counter
+    int sc = -1;
+    // Initializing the limit, to be decremented
+    int lim = n - 1;
+    //If swap counter is not equal to zero
+    while(sc != 0){
+        // set swap counter to zero for evaluation
+        sc = 0;
+        for(int i = 0; i < lim; lim--){
+            // Check if the limit reaches zero, it means that all elements are sorted at the worst case scenario, it happens
+            if(lim == 0){
+                break;
+            }
+
+            // BETA - NESTED FOR LOOP TO INCREMENT i
+            // Nested works!
+            for(int i = 0; i < lim; i++){
+                if(values[i] > values[i+1]){
+                    int tmp = values[i+1];
+                    values[i+1] = values[i];
+                    values[i] = tmp;
+                    // Elements are swapped
+
+                    // increment swap counter
+                    sc++;
+                }
+            }
+        }
+    }
+
+
     return;
 }
